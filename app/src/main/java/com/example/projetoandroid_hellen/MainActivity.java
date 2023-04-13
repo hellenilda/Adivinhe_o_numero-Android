00412ad048f9cfd4f93078128a30126070771a4d
clarasousam1 <92538328+clarasousam1@users.noreply.github.com>
2023-04-13 09:34:17 -0300

    AlertDialog

--- app/src/main/java/com/example/projetoandroid_hellen/MainActivity.java
+++ app/src/main/java/com/example/projetoandroid_hellen/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.projetoandroid_hellen;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -35,27 +37,45 @@ public class MainActivity extends AppCompatActivity {
         Random gerador = new Random();
 
         int numeroCorreto = gerador.nextInt((100-1)+1)+1;
+        Log.i("MEUAPP", "Número correto: "+numeroCorreto);
 
         btnJogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                AlertDialog.Builder alertDialog =
+                        new AlertDialog.Builder(MainActivity.this);
+
+                alertDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
                 try {
                     String strEntradaUsuario = entradaUsuario.getText().toString();
                     int intEntradaUsuario = Integer.parseInt(strEntradaUsuario);
 
-                    Log.i("MEUAPP", "Número correto: "+numeroCorreto);
                     Log.i("MEUAPP", "Número que foi digitado: "+intEntradaUsuario);
 
                     aviso.setVisibility(View.INVISIBLE);
 
                     if (entradaUsuario.getText() == null) {
-                        aviso.setVisibility(View.VISIBLE);
-                        aviso.setText("Insira um valor válido abaixo!");
+                        alertDialog
+                                .setTitle(R.string.tituloVazio);
+                        alertDialog
+                                .setMessage(R.string.mensagemVazio);
+
+                        alertDialog.create().show();
                     } else {
                         if (intEntradaUsuario < numeroCorreto) {
                             if (intEntradaUsuario < 0 | intEntradaUsuario > 100) {
-                                aviso.setVisibility(View.VISIBLE);
-                                aviso.setText("Insira um número de 0 a 100!");
+                                alertDialog
+                                        .setTitle(R.string.tituloIntervalo);
+                                alertDialog
+                                        .setMessage(R.string.mensagemIntervalo);
+                                alertDialog.create().show();
+
                                 entradaUsuario.setText(null);
                             } else {
                                 numeroMinimo.setText(String.valueOf(intEntradaUsuario));
@@ -63,26 +83,43 @@ public class MainActivity extends AppCompatActivity {
                             }
                         } else if (intEntradaUsuario > numeroCorreto) {
                             if (intEntradaUsuario < 0 | intEntradaUsuario > 100) {
-                                aviso.setVisibility(View.VISIBLE);
-                                aviso.setText("Insira um número de 0 a 100!");
+                                alertDialog
+                                        .setTitle(R.string.tituloIntervalo);
+                                alertDialog
+                                        .setMessage(R.string.mensagemIntervalo);
+                                alertDialog.create().show();
+
                                 entradaUsuario.setText(null);
                             } else {
                                 numeroMaximo.setText(String.valueOf(intEntradaUsuario));
                                 entradaUsuario.setText(null);
                             }
                         } else if (intEntradaUsuario == 0 ) {
-                            aviso.setVisibility(View.VISIBLE);
-                            aviso.setText("Digite um número maior que 0!");
-                            entradaUsuario.setText("");
+                            alertDialog
+                                    .setTitle(R.string.tituloMaiorQzero);
+                            alertDialog
+                                    .setMessage(R.string.mensagemMaiorQzero);
+                            alertDialog.create().show();
+
+                            entradaUsuario.setText(null);
                         } else if (intEntradaUsuario == numeroCorreto) {
 
                             if (intEntradaUsuario < 0 | intEntradaUsuario > 100) {
-                                aviso.setVisibility(View.VISIBLE);
-                                aviso.setText("Insira um número de 0 a 100!");
-                                entradaUsuario.setText("");
+                                alertDialog
+                                        .setTitle(R.string.tituloIntervalo);
+                                alertDialog
+                                        .setMessage(R.string.mensagemIntervalo);
+                                alertDialog.create().show();
+
+                                entradaUsuario.setText(null);
                             } else {
-                                aviso.setVisibility(View.VISIBLE);
-                                aviso.setText("PARABÉNS! VOCÊ ACERTOU O NÚMERO!");
+                                alertDialog
+                                        .setTitle(R.string.tituloAcertou);
+                                alertDialog
+                                        .setMessage(R.string.mensagemAcertou);
+
+                                alertDialog.create().show();
+
                                 btnJogar.setEnabled(false);
                                 tvIntervalo.setText(null);
                                 entradaUsuario.setText(null);
@@ -91,8 +128,12 @@ public class MainActivity extends AppCompatActivity {
                     }
                 } catch (NumberFormatException exception) {
                     Toast.makeText(MainActivity.this, R.string.erroformato, Toast.LENGTH_SHORT).show();
-                    aviso.setVisibility(View.VISIBLE);
-                    aviso.setText("Insira um valor válido abaixo!");
+                    alertDialog
+                            .setTitle(R.string.tituloVazio);
+                    alertDialog
+                            .setMessage(R.string.mensagemVazio);
+
+                    alertDialog.create().show();
                 }
             }
         });
